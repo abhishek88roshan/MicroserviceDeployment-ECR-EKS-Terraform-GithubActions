85a38df1e0455ee1694075776f407d7561df267b
EngrAbhishekRoshan <abhishek88roshan@yahoo.com>
2025-07-30 00:07:49 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,56 @@
+terraform {
+  required_version = ">= 1.3.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+resource "aws_ecr_repository" "httpd_app" {
+  name                 = "my-httpd-app"
+  image_tag_mutability = "MUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "19.21.0"
+
+  cluster_name    = var.eks_cluster_name
+  cluster_version = "1.29"
+  vpc_id          = data.aws_vpc.default.id
+  subnet_ids      = data.aws_subnets.default.ids
+
+  eks_managed_node_group_defaults = {
+    ami_type       = "AL2_x86_64"
+    disk_size      = 20
+    instance_types = [var.node_instance_type]
+  }
+
+  eks_managed_node_groups = {
+    default = {
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+    }
+  }
+}
